35ed375581ec96bf29f9a7f02381cc038df1f4be
Cem Gulec <cem.ggulecc@gmail.com>
2019-12-09 00:46:23 +0300

    initial files

---
+++ cmake-build-debug/CMakeFiles/Preemptive_Scheduling_System_with_BH.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Preemptive_Scheduling_System_with_BH.dir/main.c.obj"
+  "Preemptive_Scheduling_System_with_BH.pdb"
+  "Preemptive_Scheduling_System_with_BH.exe"
+  "Preemptive_Scheduling_System_with_BH.exe.manifest"
+  "libPreemptive_Scheduling_System_with_BH.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Preemptive_Scheduling_System_with_BH.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
